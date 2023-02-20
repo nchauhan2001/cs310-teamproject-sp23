7f1e605de1fa2ce6efde34c53d47e57bfc229411
Nidhi Chauhan <nchauhan@stu.jsu.edu>
2023-02-20 13:48:04 -0600

    My Name Added

--- src/edu/jsu/mcis/cs310/tas_sp23/Main.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/Main.java
@@ -1,6 +1,3 @@
-//Nidhi Chauhan(ScrumMaster)
-
-
 package edu.jsu.mcis.cs310.tas_sp23;
 
 import edu.jsu.mcis.cs310.tas_sp23.dao.BadgeDAO;
@@ -15,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         
         // test database connectivity; get DAOs
+        // Nidhi Chauhan
 
         DAOFactory daoFactory = new DAOFactory("tas.jdbc");
         BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
