016d5cdbb705110a25c59bcc3da6ccf7b495f227
Nidhi Chauhan <nchauhan@stu.jsu.edu>
2023-02-21 19:35:59 -0600

    My Name is Added

--- src/edu/jsu/mcis/cs310/tas_sp23/Main.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public static void main(String[] args) {
         
         // test database connectivity; get DAOs
-        // Nidhi Chauhan
+// Nidhi Chauhan
 
         DAOFactory daoFactory = new DAOFactory("tas.jdbc");
         BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
