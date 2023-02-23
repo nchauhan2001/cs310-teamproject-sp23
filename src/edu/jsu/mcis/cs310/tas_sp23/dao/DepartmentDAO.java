d922e90365bb96d9ede8d382d8c18ab344382e24
guthr <kguthrie2@stu.jsu.edu>
2023-02-23 04:21:55 -0600

    Added find method into DepartmentDAO

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/DepartmentDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/DepartmentDAO.java
@@ -1,5 +1,80 @@
 package edu.jsu.mcis.cs310.tas_sp23.dao;
 
+import edu.jsu.mcis.cs310.tas_sp23.Department;
+import java.sql.*;
+
 public class DepartmentDAO {
     
+    private static final String QUERY_FIND = "SELECT * FROM department WHERE id = ?";
+
+    private final DAOFactory daoFactory;
+
+    DepartmentDAO(DAOFactory daoFactory) {
+
+        this.daoFactory = daoFactory;
+
+    }
+    
+    public Department find(int id) {
+        
+        Department department = null;
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            Connection conn = daoFactory.getConnection();
+
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_FIND);
+                ps.setString(1, String.valueOf(id));
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+
+                    while (rs.next()) {
+
+                        String description = rs.getString("description");
+                        int terminalid = rs.getInt("terminalid");
+
+                        department = new Department(id, description, terminalid);
+
+                    }
+
+                }
+
+            }
+
+        } catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+
+        return department;
+
+    }
+
 }
