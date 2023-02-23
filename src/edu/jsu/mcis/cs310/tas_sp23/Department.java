bfe072d0268aa4dc8ac37eef2fdea17f95247ed2
guthr <kguthrie2@stu.jsu.edu>
2023-02-23 04:06:10 -0600

    Added constructor, getter methods, and toString override to Department

--- src/edu/jsu/mcis/cs310/tas_sp23/Department.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/Department.java
@@ -2,4 +2,36 @@ package edu.jsu.mcis.cs310.tas_sp23;
 
 public class Department {
 
+    private final int id, terminalId;
+    private final String description;
+    
+    public Department(int id, String desc, int termid) {
+        this.id = id;
+        this.description = desc;
+        this.terminalId = termid;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public int getTerminalId() {
+        return terminalId;
+    }
+    
+    @Override
+    public String toString() {
+        
+        StringBuilder s = new StringBuilder();
+        
+        s.append('#').append(id).append(' ');
+        s.append('(').append(description).append("), ");
+        s.append("Terminal ID: ").append(terminalId);
+        
+        return s.toString(); 
+    }
 }
