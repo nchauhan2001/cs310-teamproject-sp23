1028784f140f3ae2fe391d18fc1f66dd83683dd5
wahickssware84 <wahickssware84@gmail.com>
2023-02-24 12:31:39 -0600

    Began work on Shift.java. Created instance fields and the skeleton for HashMap.

---
+++ src/edu/jsu/mcis/cs310/tas_sp23/Shift.java
@@ -0,0 +1,17 @@
+package edu.jsu.mcis.cs310.tas_sp23;
+
+import java.util.*;
+import java.time.*;
+
+public class Shift {
+
+    private String desc;
+    private LocalTime startingTime, stoppingTime, lunchStart, lunchStop;
+    // These are the parameters for a given emplyee's shift. 
+    // N.B. These are to be measured in minutes!!! - William H.
+    private Duration lunchduration, shiftduration;
+
+    public Shift(HashMap map) {
+        // UNFINISHED; code will go here. - William H.
+    }
+}
