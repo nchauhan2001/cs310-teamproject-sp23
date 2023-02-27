742604bf63828a0b2ca2f451fbbf136e6c1d6d94
wahickssware84 <wahickssware84@gmail.com>
2023-02-27 12:29:22 -0600

    Finished Shift.java (for now, anyway).

--- src/edu/jsu/mcis/cs310/tas_sp23/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/Shift.java
@@ -6,12 +6,42 @@ import java.time.*;
 public class Shift {
 
     private String desc;
-    private LocalTime startingTime, stoppingTime, lunchStart, lunchStop;
+    private LocalTime startTime, stopTime, lunchStart, lunchStop;
     // These are the parameters for a given emplyee's shift. 
     // N.B. These are to be measured in minutes!!! - William H.
     private Duration lunchduration, shiftduration;
 
     public Shift(HashMap map) {
-        // UNFINISHED; code will go here. - William H.
+        // N.B. I will double-check this HashMap later to ensure that it is fininished (for now, anyways). - William H.
+        this.startTime = LocalTime.parse((String) map.get("startTime"));
+        this.stopTime = LocalTime.parse((String) map.get("stopTime"));
+        this.lunchStart = LocalTime.parse((String) map.get("lunchStart"));
+        this.lunchStop = LocalTime.parse((String) map.get("lunchStop"));
+        this.lunchduration = Duration.between(lunchStart, lunchStop);
+        this.shiftduration = Duration.between(startTime, stopTime);
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getStopTime() {
+        return stopTime;
+    }
+
+    public LocalTime getLunchStart() {
+        return lunchStart;
+    }
+
+    public LocalTime getLunchStop() {
+        return lunchStop;
+    }
+
+    public Duration getLunchDuration() {
+        return lunchduration;
+    }
+
+    public Duration getShiftDuration() {
+        return shiftduration;
     }
 }
