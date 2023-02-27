1bcdd96daf05194ff9f1ac5fc16f7e4ec53e0a06
Kolton <kguthrie2@stu.jsu.edu>
2023-02-27 13:27:57 -0600

    Added toString method

--- src/edu/jsu/mcis/cs310/tas_sp23/Employee.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/Employee.java
@@ -1,6 +1,7 @@
 package edu.jsu.mcis.cs310.tas_sp23;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Employee {
     
@@ -25,6 +26,27 @@ public class Employee {
         this.department = department;
         this.shift = shift;
         this.employeeType = employeeType;
+        
+    }
+    
+    @Override
+    public String toString() {
+        
+        // Formats the date in the correct format
+        // MM: month-of-year / dd: day-of-month / yyyy: year-of-era
+        // Docs: https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+        final String date = DateTimeFormatter.ofPattern("MM/dd/yyyy").format(active).toUpperCase();
+
+        StringBuilder s = new StringBuilder();
+
+        s.append("ID #").append(id).append(": ");
+        s.append(lastName).append(", ").append(firstName).append(" ").append(middleName);
+        s.append(" (#").append(badge.getId()).append("), ");
+        s.append("Type: ").append(employeeType.toString()).append(", ");
+        s.append("Department: ").append(department.getDescription()).append(", ");
+        s.append("Active: ").append(date);
+        
+        return s.toString();
         
     }
 }
