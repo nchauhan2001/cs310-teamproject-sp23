b0c5b7f5e1dc3524ee1e444b98a9aebfbcb53918
Jonathan Cade Hardin <jhardin5@stu.jsu.edu>
2023-02-27 13:14:36 -0600

    started final test for EmployeeFindtest

--- test/edu/jsu/mcis/cs310/tas_sp23/EmployeeFindTest.java
+++ test/edu/jsu/mcis/cs310/tas_sp23/EmployeeFindTest.java
@@ -81,4 +81,13 @@ public class EmployeeFindTest {
 
     }
     
+    @Test 
+    public void testFindEmployee4() {
+        
+        EmployeeDAO employeeDAO = daoFactory.getEmployeeDAO();
+        
+        Employee e5 = EmployeeDAO.find(115);
+        
+        assertEquals("ID #115: Insert DB info here",e5.toString());
+    }
 }
