22c2631fc8927155b3db890827a50c7f4f9aa1d0
guthr <kguthrie2@stu.jsu.edu>
2023-03-01 17:32:54 -0600

    Updated variable names

--- test/edu/jsu/mcis/cs310/tas_sp23/PunchCreateTest.java
+++ test/edu/jsu/mcis/cs310/tas_sp23/PunchCreateTest.java
@@ -38,9 +38,9 @@ public class PunchCreateTest {
         /* Get Punch Properties */
         
         String badgeid = p1.getBadge().getId();
-        ots = p1.getOriginaltimestamp();
-        int terminalid = p1.getTerminalid();
-        EventType punchtype = p1.getPunchtype();
+        ots = p1.getOriginalTimestamp();
+        int terminalid = p1.getTerminalId();
+        EventType punchtype = p1.getPunchType();
 
         /* Insert Punch Into Database */
         
@@ -54,10 +54,10 @@ public class PunchCreateTest {
         
         assertEquals(badgeid, p2.getBadge().getId());
 
-        rts = p2.getOriginaltimestamp();
+        rts = p2.getOriginalTimestamp();
 
-        assertEquals(terminalid, p2.getTerminalid());
-        assertEquals(punchtype, p2.getPunchtype());
+        assertEquals(terminalid, p2.getTerminalId());
+        assertEquals(punchtype, p2.getPunchType());
         assertEquals(ots.format(dtf), rts.format(dtf));
 
     }
