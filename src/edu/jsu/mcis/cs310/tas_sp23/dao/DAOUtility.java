5895cfa1ec4249ca9e1775de96055369223b0cbd
Matthew Hayes <mhayes6@stu.jsu.edu>
2023-03-03 22:58:29 -0600

    Added getPunchListAsJSON method to DAOUtility.java

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/DAOUtility.java
@@ -5,6 +5,7 @@ import java.util.*;
 import java.time.temporal.ChronoUnit;
 import java.time.format.DateTimeFormatter;
 import com.github.cliftonlabs.json_simple.*;
+import edu.jsu.mcis.cs310.tas_sp23.Punch;
 
 /**
  * 
@@ -14,5 +15,35 @@ import com.github.cliftonlabs.json_simple.*;
  * 
  */
 public final class DAOUtility {
+    
+    public static String getPunchListAsJSON(ArrayList<Punch> punchList) {
+ 
+        String dateFormat = "E MM/dd/yyyy HH:mm:ss";
+        
+        /* Create ArrayList Object */
+        ArrayList<HashMap<String, String>> jsonData = new ArrayList<>();
 
+        /* Create HashMap Object (one for every Punch!) */
+        for (int i = 0; i < punchList.size(); i++){
+            HashMap<String, String>  punchData = new HashMap<>();
+            Punch punch = punchList.get(i);
+
+            /* Add Punch Data to HashMap */
+            punchData.put("id", String.valueOf(punch.getId()));
+            punchData.put("badgeid", punch.getBadge().getId());
+            punchData.put("terminalid", String.valueOf(punch.getTerminalId()));
+            punchData.put("punchtype", punch.getPunchType().toString());
+            punchData.put("adjustmenttype", punch.getAdjustmentType().toString());
+            
+            String date = DateTimeFormatter.ofPattern(dateFormat).format(punch.getOriginalTimestamp()).toUpperCase();
+            punchData.put("originaltimestamp", date);
+            
+            String adjustedTime = DateTimeFormatter.ofPattern(dateFormat).format(punch.getAdjustedTimestamp()).toUpperCase();
+            punchData.put("adjustedtimestamp", adjustedTime);
+
+            /* Append HashMap to ArrayList */
+            jsonData.add(punchData);
+        }
+        return Jsoner.serialize(jsonData);
+    }
 }
