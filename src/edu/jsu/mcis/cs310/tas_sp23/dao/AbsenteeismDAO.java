d74def8e8e850dfb4d09e3100bb0b1aa7f4b65b1
Kolton <kguthrie2@stu.jsu.edu>
2023-03-08 13:20:18 -0600

    Implemented the find() method

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/AbsenteeismDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/AbsenteeismDAO.java
@@ -1,5 +1,80 @@
 package edu.jsu.mcis.cs310.tas_sp23.dao;
 
+import edu.jsu.mcis.cs310.tas_sp23.Employee;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.sql.*;
+
 public class AbsenteeismDAO {
+
+    private final DAOFactory daoFactory;
+    
+    private static final String QUERY_FIND = "SELECT * FROM absenteeism WHERE employeeid = ? AND payperiod = ?";
+
+    AbsenteeismDAO( DAOFactory daoFactory) {
+
+        this.daoFactory = daoFactory;
+
+    }
+    
+    public Absenteeism find(Employee employee, LocalDate localDate) {
+        Absenteeism absenteeism = null;
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            Connection conn = daoFactory.getConnection();
+
+            if (conn.isValid(0)) {
+
+                ps = conn.prepareStatement(QUERY_FIND);
+                ps.setInt(1, employee.getId());
+                ps.setString(2, localDate.toString());
+
+                boolean hasresults = ps.execute();
+
+                if (hasresults) {
+
+                    rs = ps.getResultSet();
+
+                    while (rs.next()) {
+
+                        BigDecimal percentage = rs.getBigDecimal("percentage");
+                        absenteeism = new Absenteeism(employee, localDate, percentage);
+
+                    }
+
+                }
+
+            }
+
+        } catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+
+        }
+
+        return absenteeism;
+
+    }
     
 }
