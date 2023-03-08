189e8aa9d38dd8b1b3663136cb368ffdb105f2ae
Kolton <kguthrie2@stu.jsu.edu>
2023-03-08 12:52:08 -0600

    Added getAbsenteeismDAO()

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/DAOFactory.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/DAOFactory.java
@@ -51,5 +51,9 @@ public final class DAOFactory {
     public DepartmentDAO getDepartmentDAO() {
         return new DepartmentDAO(this);
     }
+    
+    public AbsenteeismDAO getAbsenteeismDAO() {
+        return new AbsenteeismDAO(this);
+    }
 
 }
