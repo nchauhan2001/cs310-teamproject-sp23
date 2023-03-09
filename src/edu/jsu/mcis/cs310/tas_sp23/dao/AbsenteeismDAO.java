aa1d691af24f559c1140018c7bdf01b08dae799e
Kolton <kguthrie2@stu.jsu.edu>
2023-03-09 09:30:33 -0600

    Finished implementing create() method

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/AbsenteeismDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/AbsenteeismDAO.java
@@ -1,5 +1,6 @@
 package edu.jsu.mcis.cs310.tas_sp23.dao;
 
+import edu.jsu.mcis.cs310.tas_sp23.Absenteeism;
 import edu.jsu.mcis.cs310.tas_sp23.Employee;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -10,6 +11,8 @@ public class AbsenteeismDAO {
     private final DAOFactory daoFactory;
     
     private static final String QUERY_FIND = "SELECT * FROM absenteeism WHERE employeeid = ? AND payperiod = ?";
+    private static final String QUERY_UPDATE = "UPDATE absenteeism SET percentage = ? WHERE employeeid = ? AND payperiod = ?";
+    private static final String QUERY_CREATE = "INSET INTO absenteeism (employeeid, payperiod, percentage) VALUES (?, ?, ?)";
 
     AbsenteeismDAO( DAOFactory daoFactory) {
 
@@ -28,10 +31,10 @@ public class AbsenteeismDAO {
             Connection conn = daoFactory.getConnection();
 
             if (conn.isValid(0)) {
-
+                                   
                 ps = conn.prepareStatement(QUERY_FIND);
                 ps.setInt(1, employee.getId());
-                ps.setString(2, localDate.toString());
+                ps.setDate(2, Date.valueOf(localDate));
 
                 boolean hasresults = ps.execute();
 
@@ -78,7 +81,67 @@ public class AbsenteeismDAO {
     }
     
     public int create(Absenteeism absenteeism) {
-        return 0;
+        
+        int key = 0;
+            
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+                    
+            try {
+                
+            Connection conn = daoFactory.getConnection();
+
+                if (conn.isValid(0)) {
+                    
+                    // Find if record already exists
+                    if(find(absenteeism.getEmployee(), absenteeism.getLocalDate()) != null) {
+            
+                        ps = conn.prepareStatement(QUERY_UPDATE, PreparedStatement.RETURN_GENERATED_KEYS);
+                        ps.setBigDecimal(1, absenteeism.getPercentage());
+                        ps.setInt(2, absenteeism.getEmployee().getId());
+                        ps.setDate(3, Date.valueOf(absenteeism.getLocalDate()));
+                        
+                    } else { // If record does not exists already
+                        ps = conn.prepareStatement(QUERY_CREATE, PreparedStatement.RETURN_GENERATED_KEYS);
+                        ps.setInt(1, absenteeism.getEmployee().getId());
+                        ps.setDate(2, Date.valueOf(absenteeism.getLocalDate()));
+                        ps.setBigDecimal(3, absenteeism.getPercentage());
+                    }
+                    
+                    int result = ps.executeUpdate();
+                    if (result > 0) {
+
+                        rs = ps.getGeneratedKeys();
+                        if (rs.next()) {
+                            key = rs.getInt(1);
+                        }
+
+                    }
+                }
+
+            } catch (SQLException e) {
+
+                throw new DAOException(e.getMessage());
+
+            } finally {
+                if (rs != null) {
+                    try {
+                        rs.close();
+                    } catch (SQLException e) {
+                        throw new DAOException(e.getMessage());
+                    }
+                }
+                if (ps != null) {
+                    try {
+                        ps.close();
+                    } catch (SQLException e) {
+                        throw new DAOException(e.getMessage());
+                    }
+                }
+            }         
+        
+        return key;
+        
     }
     
 }
