f05f1bcfb1acb2f844e6d03774154d625b40e532
Matthew Hayes <mhayes6@stu.jsu.edu>
2023-03-13 12:52:41 -0500

    Fixes for ShiftFindTest.java

--- test/edu/jsu/mcis/cs310/tas_sp23/ShiftFindTest.java
+++ test/edu/jsu/mcis/cs310/tas_sp23/ShiftFindTest.java
@@ -63,45 +63,73 @@ public class ShiftFindTest {
     }
     
     @Test
-    public void testFindShiftByDepartment(){
-        
+    public void testFindShiftByID2() {
+
         ShiftDAO shiftDAO = daoFactory.getShiftDAO();
-        DepartmentDAO departmentDAO = daoFactory.getDepartmentDAO();
+
+        /* Retrieve Shift Rulesets from Database */
         
-        //create department objects
-        Department d1 = departmentDAO.find("Assembly");
-        Department d2 = departmentDAO.find("Cleaning");
-        Department d3 = departmentDAO.find("Warehouse");
+        Shift s1 = shiftDAO.find(2);
+        Shift s2 = shiftDAO.find(4);
+        Shift s3 = shiftDAO.find(1);
+
+        /* Compare to Expected Values */
         
-        //retrieve shift rulesets from the database
-        
-        Shift s1 = shiftDAO.find(d1);
-        Shift s2 = shiftDAO.find(d2);
-        Shift s3 = shiftDAO.find(d3);
-        
-        //Compare to expected equals
-        
-        assertEquals("Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)", s1.toString());
-        assertEquals("Shift 2: 12:00 - 20:30 (510 minutes); Lunch: 16:30 - 17:00 (30 minutes)", s2.toString());
-        assertEquals("Shift 1 Early Lunch: 07:00 - 15:30 (510 minutes); Lunch: 11:30 - 12:00 (30 minutes)", s3.toString());
+        assertEquals("Shift 2: 12:00 - 20:30 (510 minutes); Lunch: 16:30 - 17:00 (30 minutes)", s1.toString());
+        assertEquals("Shift 3: 22:30 - 07:00 (510 minutes); Lunch: 02:30 - 03:00 (30 minutes)", s2.toString());
+        assertEquals("Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)", s3.toString());
+
     }
 
     @Test
-    public void testFindShiftByEmployee(){
-        
+    public void testFindShiftByBadge2() {
+
         ShiftDAO shiftDAO = daoFactory.getShiftDAO();
-        EmployeeDAO employeeDAO = daoFactory.getEmployeeDAO();
+        BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
+
+        /* Create Badge Objects */
         
-        //create employee objects
-        Employee e1 = employeeDAO.find("");
-        Employee e2 = employeeDAO.find("");
-        Employee e3 = employeeDAO.find("");
+        Badge b1 = badgeDAO.find("F1EE0555");
+        Badge b2 = badgeDAO.find("28DC3FB8");
+        Badge b3 = badgeDAO.find("08D01475");
+
+        /* Retrieve Shift Rulesets from Database */
+        
+        Shift s1 = shiftDAO.find(b1);
+        Shift s2 = shiftDAO.find(b2);
+        Shift s3 = shiftDAO.find(b3);
+
+        /* Compare to Expected Values */
+        
+        assertEquals("Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)", s1.toString());
+        assertEquals("Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)", s2.toString());
+        assertEquals("Shift 2: 12:00 - 20:30 (510 minutes); Lunch: 16:30 - 17:00 (30 minutes)", s3.toString());
+
+    }
+
+    @Test
+    public void testFindShiftByBadge3() {
+
+        ShiftDAO shiftDAO = daoFactory.getShiftDAO();
+        BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
+
+        /* Create Badge Objects */
+        
+        Badge b1 = badgeDAO.find("408B195F");
+        Badge b2 = badgeDAO.find("398B1563");
+        Badge b3 = badgeDAO.find("ADD650A8");
+
+        /* Retrieve Shift Rulesets from Database */
+        
+        Shift s1 = shiftDAO.find(b1);
+        Shift s2 = shiftDAO.find(b2);
+        Shift s3 = shiftDAO.find(b3);
+
+        /* Compare to Expected Values */
         
         assertEquals("Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)", s1.toString());
         assertEquals("Shift 2: 12:00 - 20:30 (510 minutes); Lunch: 16:30 - 17:00 (30 minutes)", s2.toString());
-        assertEquals("Shift 1 Early Lunch: 07:00 - 15:30 (510 minutes); Lunch: 11:30 - 12:00 (30 minutes)", s3.toString());
-        
-        
-        
+        assertEquals("Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)", s3.toString());
+
     }
 }
