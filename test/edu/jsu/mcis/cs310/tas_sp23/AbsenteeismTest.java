a7938e78c50420d8d1efbbbc1472442a9180ec1a
Matthew Hayes <mhayes6@stu.jsu.edu>
2023-03-17 00:58:05 -0500

    Added more tests to AbsenteeismTest.java

--- test/edu/jsu/mcis/cs310/tas_sp23/AbsenteeismTest.java
+++ test/edu/jsu/mcis/cs310/tas_sp23/AbsenteeismTest.java
@@ -127,7 +127,7 @@ public class AbsenteeismTest {
 
         /* Get Punch/Employee Objects */
         
-        Punch p = punchDAO.find(4943);
+        Punch p = punchDAO.find(1975);
         Employee e = employeeDAO.find(p.getBadge());
         Shift s = e.getShift();
         Badge b = e.getBadge();
@@ -161,8 +161,101 @@ public class AbsenteeismTest {
         
         /* Compare to Expected Value */
         
-        assertEquals("#08D01475 (Pay Period Starting 09-16-2018): -27.50%", a2.toString());
+        assertEquals("#8C0644BA (Pay Period Starting 08-19-2018): -40.63%", a2.toString());
         
     }
     
+    @Test
+    public void testAbsenteeismShift2Weekday() {
+        
+        AbsenteeismDAO absenteeismDAO = daoFactory.getAbsenteeismDAO();
+        EmployeeDAO employeeDAO = daoFactory.getEmployeeDAO();
+        PunchDAO punchDAO = daoFactory.getPunchDAO();
+		
+        /* Get Punch/Employee Objects */
+        
+        Punch p = punchDAO.find(3452);
+        Employee e = employeeDAO.find(p.getBadge());
+        Shift s = e.getShift();
+        Badge b = e.getBadge();
+        
+        /* Get Pay Period Punch List */
+        
+        LocalDate ts = p.getOriginalTimestamp().toLocalDate();
+        LocalDate begin = ts.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
+        LocalDate end = begin.with(TemporalAdjusters.next(DayOfWeek.SATURDAY));
+        
+        ArrayList<Punch> punchlist = punchDAO.list(b, begin, end);
+        
+        /* Adjust Punch List */
+        
+        for (Punch punch : punchlist) {
+            punch.adjust(s);
+        }
+        
+        /* Compute Pay Period Total Absenteeism */
+        
+        BigDecimal percentage = DAOUtility.calculateAbsenteeism(punchlist, s);
+        
+        /* Insert Absenteeism Into Database */
+        
+        Absenteeism a1 = new Absenteeism(e, ts, percentage);
+        absenteeismDAO.create(a1);
+        
+        /* Retrieve Absenteeism From Database */
+        
+        Absenteeism a2 = absenteeismDAO.find(e, ts);
+        
+        /* Compare to Expected Value */
+        
+        assertEquals("#E06BE060 (Pay Period Starting 09-02-2018): -7.50%", a2.toString());
+        
+    }
+    
+    @Test
+    public void testAbsenteeismShift3Weekend() {
+        
+        AbsenteeismDAO absenteeismDAO = daoFactory.getAbsenteeismDAO();
+        EmployeeDAO employeeDAO = daoFactory.getEmployeeDAO();
+        PunchDAO punchDAO = daoFactory.getPunchDAO();
+
+        /* Get Punch/Employee Objects */
+        
+        Punch p = punchDAO.find(3072);
+        Employee e = employeeDAO.find(p.getBadge());
+        Shift s = e.getShift();
+        Badge b = e.getBadge();
+        
+        /* Get Pay Period Punch List */
+        
+        LocalDate ts = p.getOriginalTimestamp().toLocalDate();
+        LocalDate begin = ts.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
+        LocalDate end = begin.with(TemporalAdjusters.next(DayOfWeek.SATURDAY));
+        
+        ArrayList<Punch> punchlist = punchDAO.list(b, begin, end);
+        
+        /* Adjust Punch List */
+        
+        for (Punch punch : punchlist) {
+            punch.adjust(s);
+        }
+        
+        /* Compute Pay Period Total Absenteeism */
+        
+        BigDecimal percentage = DAOUtility.calculateAbsenteeism(punchlist, s);
+        
+        /* Insert Absenteeism Into Database */
+        
+        Absenteeism a1 = new Absenteeism(e, ts, percentage);
+        absenteeismDAO.create(a1);
+        
+        /* Retrieve Absenteeism From Database */
+        
+        Absenteeism a2 = absenteeismDAO.find(e, ts);
+        
+        /* Compare to Expected Value */
+        
+        assertEquals("#DD6E2C0C (Pay Period Starting 08-26-2018): -15.00%", a2.toString());
+        
+    }
 }
