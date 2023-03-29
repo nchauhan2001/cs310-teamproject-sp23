2a372967bd2af6349db213e6a9546f0b1ae4312b
Matthew Hayes <mhayes6@stu.jsu.edu>
2023-03-29 13:10:22 -0500

    Added dailyschedule object to shift class with getter

--- src/edu/jsu/mcis/cs310/tas_sp23/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/Shift.java
@@ -11,31 +11,13 @@ public class Shift {
     // These are the parameters for a given emplyee's shift. 
     // N.B. These are to be measured in minutes!!! - William H.
     private Duration lunchduration, shiftduration;
+    private DailySchedule defaultschedule;
 
     public Shift(HashMap<String, String> map) {
+        
         this.id = Integer.parseInt(map.get("id"));
         this.desc = map.get("description");
-        this.startTime = LocalTime.parse( map.get("startTime"));
-        this.stopTime = LocalTime.parse( map.get("stopTime"));
-        this.gracePeriod = Integer.parseInt(map.get("gracePeriod"));
-        this.dockPenalty = Integer.parseInt(map.get("dockPenalty"));
-        this.roundInterval = Integer.parseInt(map.get("roundInterval"));
-        this.lunchStart = LocalTime.parse( map.get("lunchStart"));
-        this.lunchStop = LocalTime.parse( map.get("lunchStop"));
-        this.lunchThreshold = Integer.parseInt(map.get("lunchThreshold"));
         
-        this.lunchduration = Duration.between(lunchStart, lunchStop);
-        
-        if (desc.equals("Shift 3")){
-            this.shiftduration = Duration.between(startTime, stopTime);
-            LocalTime timeDifference = (LocalTime)this.shiftduration.addTo(LocalTime.MAX);
-            this.shiftduration = Duration.ofHours(timeDifference.getHour()).plus(this.lunchduration);
-            System.out.println(this.shiftduration);
-        }
-        else{
-            this.shiftduration = Duration.between(startTime, stopTime);
-
-        }
         
     }
     // WEBHOOK TEST
@@ -85,6 +67,10 @@ public class Shift {
 
     public Duration getShiftDuration() {
         return shiftduration;
+    }
+    
+    public DailySchedule getDefaultschedule() {
+        return defaultschedule;
     }
 
     @Override
