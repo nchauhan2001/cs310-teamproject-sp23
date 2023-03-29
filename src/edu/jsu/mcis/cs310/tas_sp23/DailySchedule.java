b34911f3c2901e5e90c3519d7f231873c075e5be
Kolton <kguthrie2@stu.jsu.edu>
2023-03-29 13:11:48 -0500

    Updated DailySchedule

--- src/edu/jsu/mcis/cs310/tas_sp23/DailySchedule.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/DailySchedule.java
@@ -1,22 +1,39 @@
 package edu.jsu.mcis.cs310.tas_sp23;
 
+import java.time.Duration;
 import java.time.LocalTime;
+import java.util.HashMap;
 
 public class DailySchedule {
     
     private LocalTime startTime, stopTime, lunchStart, lunchStop;
-    private int lunchThreshold, roundInterval, gracePeriod, dockPenalty;;
+    private int lunchThreshold, roundInterval, gracePeriod, dockPenalty;
+    private Duration lunchduration, shiftduration;
 
-    public DailySchedule(LocalTime startTime, LocalTime stopTime, LocalTime lunchStart, LocalTime lunchStop, int roundInterval, int gracePeriod, int dockPenalty, int lunchThreshold) {
+    public DailySchedule(HashMap<String, String> map) {
         
-        this.startTime = startTime;
-        this.stopTime = stopTime;
-        this.lunchStart = lunchStart;
-        this.lunchStop = lunchStop;
-        this.roundInterval = roundInterval;
-        this.gracePeriod = gracePeriod;
-        this.dockPenalty = dockPenalty;
-        this.lunchThreshold = lunchThreshold;
+        String desc = map.get("description");
+       
+        this.startTime = LocalTime.parse( map.get("startTime"));
+        this.stopTime = LocalTime.parse( map.get("stopTime"));
+        this.gracePeriod = Integer.parseInt(map.get("gracePeriod"));
+        this.dockPenalty = Integer.parseInt(map.get("dockPenalty"));
+        this.roundInterval = Integer.parseInt(map.get("roundInterval"));
+        this.lunchStart = LocalTime.parse( map.get("lunchStart"));
+        this.lunchStop = LocalTime.parse( map.get("lunchStop"));
+        this.lunchThreshold = Integer.parseInt(map.get("lunchThreshold"));
+        
+        this.lunchduration = Duration.between(lunchStart, lunchStop);
+        
+        if (desc.equals("Shift 3")){
+            this.shiftduration = Duration.between(startTime, stopTime);
+            LocalTime timeDifference = (LocalTime)this.shiftduration.addTo(LocalTime.MAX);
+            this.shiftduration = Duration.ofHours(timeDifference.getHour()).plus(this.lunchduration);
+            System.out.println(this.shiftduration);
+        }
+        else{
+            this.shiftduration = Duration.between(startTime, stopTime);
+        }
         
     }
     
