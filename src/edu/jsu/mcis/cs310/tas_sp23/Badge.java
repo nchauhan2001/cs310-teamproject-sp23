be7daf310f4d021b15a6f5ad046ed20161227e10
Matthew Hayes <mhayes6@stu.jsu.edu>
2023-04-06 19:54:13 -0500

    Added constructor that generates an ID for the given employee

--- src/edu/jsu/mcis/cs310/tas_sp23/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/Badge.java
@@ -1,5 +1,7 @@
 package edu.jsu.mcis.cs310.tas_sp23;
 
+import java.util.zip.CRC32;
+
 public class Badge {
 
     private final String id, description;
@@ -7,6 +9,28 @@ public class Badge {
     public Badge(String id, String description) {
         this.id = id;
         this.description = description;
+    }
+    
+    public Badge(String description) {
+        this.description = description;
+        byte[] b = this.description.getBytes();
+
+        CRC32 c = new CRC32();
+        
+        for (int b1: b){
+            c.update(b1);  
+        }
+        
+        String genId = Long.toHexString(c.getValue());
+        int diff = 8 - genId.length();
+
+        while (diff > 0){
+            String formatId = String.format("0%1s", genId);
+            genId = formatId;
+            diff--;
+
+        }
+        this.id = genId.toUpperCase();
     }
 
     public String getId() {
