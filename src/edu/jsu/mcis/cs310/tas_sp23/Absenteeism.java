43042aa3083a69182d125c008a25230e8c77751c
Jonathan Cade Hardin <jhardin5@stu.jsu.edu>
2023-04-10 13:12:53 -0500

    finished documentation for Absenteeism.java

--- src/edu/jsu/mcis/cs310/tas_sp23/Absenteeism.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/Absenteeism.java
@@ -4,28 +4,56 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ *<p> This feature provides an Absenteeism model object class which will be used to represent an employee's absenteeism record for a specific pay period.</p> 
+ * @author Team C
+ */
 public class Absenteeism {
     
     private Employee employee;
     private LocalDate payPeriod;
     private BigDecimal percentage;
     
+    /**
+     *
+     * @param employee the employee's badge number 
+     * @param localDate starting pay period
+     * @param percentage percentage of time the employee missed
+     */
     public Absenteeism(Employee employee, LocalDate localDate, BigDecimal percentage){
         this.employee = employee;
         this.payPeriod = localDate;
         this.percentage = percentage;
     }
     
+    /**
+     *<p> getter method 'getEmployee' returns employee </p>
+     * @return
+     */
     public Employee getEmployee(){
         return employee;
     }
+
+    /**
+     *<p> getter method 'getPayPeriod' returns PayPeriod </p>
+     * @return
+     */
     public LocalDate getPayPeriod(){
         return payPeriod;
     }
+
+    /**
+     *<p> getter method 'getPercentage' returns percentage </p>
+     * @return
+     */
     public BigDecimal getPercentage(){
         return percentage;
     }
     
+    /**
+     *<p> toString method that returns s as a string </p>
+     * @return
+     */
     @Override
     public String toString(){
         StringBuilder s = new StringBuilder();
