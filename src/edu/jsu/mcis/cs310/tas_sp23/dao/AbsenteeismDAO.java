fa5c15c7c62e7b24ad597a952b673e6104a5452a
unknown <wahickssware84@gmail.com>
2023-04-14 09:00:14 -0500

    Formatted API commentary in Absenteeism.java.

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/AbsenteeismDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/AbsenteeismDAO.java
@@ -8,46 +8,64 @@ import java.time.LocalDate;
 import java.sql.*;
 import java.time.DayOfWeek;
 import java.time.temporal.TemporalAdjusters;
+
 /**
- * <p> JAVADOC - CLASS </p>
+ * <p>
+ * JAVADOC - CLASS </p>
+ *
  * @author wahic
  */
 public class AbsenteeismDAO {
-   /**
-    * <p> JAVADOC - FIELD </p>
-    * @author wahic
-    */
+
+    /**
+     * <p>
+     * JAVADOC - FIELD </p>
+     *
+     * @author wahic
+     */
     private final DAOFactory daoFactory;
-   /**
-    * <p> JAVADOC - FIELD </p>
-    * @author wahic
-    */
+    /**
+     * <p>
+     * JAVADOC - FIELD </p>
+     *
+     * @author wahic
+     */
     private static final String QUERY_FIND = "SELECT * FROM absenteeism WHERE employeeid = ? AND payperiod = ?";
-   /**
-    * <p> JAVADOC - FIELD </p>
-    * @author wahic
-    */
+    /**
+     * <p>
+     * JAVADOC - FIELD </p>
+     *
+     * @author wahic
+     */
     private static final String QUERY_UPDATE = "UPDATE absenteeism SET percentage = ? WHERE employeeid = ? AND payperiod = ?";
-   /**
-    * <p> JAVADOC - FIELD </p>
-    * @author wahic
-    */    
+    /**
+     * <p>
+     * JAVADOC - FIELD </p>
+     *
+     * @author wahic
+     */
     private static final String QUERY_CREATE = "INSERT INTO absenteeism (employeeid, payperiod, percentage) VALUES (?, ?, ?)";
-   /**
-    * <p> JAVADOC - METHOD </p>
-    * @author wahic
-    */
-    AbsenteeismDAO( DAOFactory daoFactory) {
+
+    /**
+     * <p>
+     * JAVADOC - METHOD </p>
+     *
+     * @author wahic
+     */
+    AbsenteeismDAO(DAOFactory daoFactory) {
 
         this.daoFactory = daoFactory;
 
     }
-   /**
-    * <p> JAVADOC - METHOD </p>
-    * @param x
-    * @return y
-    * @author wahic
-    */
+
+    /**
+     * <p>
+     * JAVADOC - METHOD </p>
+     *
+     * @param x
+     * @return y
+     * @author wahic
+     */
     public Absenteeism find(Employee employee, LocalDate localDate) {
         Absenteeism absenteeism = null;
 
@@ -59,14 +77,14 @@ public class AbsenteeismDAO {
             Connection conn = daoFactory.getConnection();
 
             if (conn.isValid(0)) {
-                                   
+
                 ps = conn.prepareStatement(QUERY_FIND);
                 ps.setInt(1, employee.getId());
                 LocalDate payPeriod = localDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
                 ps.setDate(2, Date.valueOf(payPeriod));
-               /**
-                * <[> JAVADOC - FIELD </p>
-                */
+                /**
+                 * <[> JAVADOC - FIELD </p>
+                 */
                 boolean hasresults = ps.execute();
 
                 if (hasresults) {
@@ -107,87 +125,96 @@ public class AbsenteeismDAO {
             }
 
         }
-       /**
-        * <p> JAVADOC - TYPE </p>
-        * @author wahic
-        */
+        /**
+         * <p>
+         * JAVADOC - TYPE </p>
+         *
+         * @author wahic
+         */
         return absenteeism;
 
     }
+
     /**
-     * <p> JAVADOC - TYPE </p>
+     * <p>
+     * JAVADOC - TYPE </p>
+     *
      * @author wahic
      */
     public int create(Absenteeism absenteeism) {
-        
+
         int key = 0;
-            
-            PreparedStatement ps = null;
-            ResultSet rs = null;
-                    
-            try {
-                
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
             Connection conn = daoFactory.getConnection();
 
-                if (conn.isValid(0)) {
-                    
-                    // Find if record already exists
-                    if(find(absenteeism.getEmployee(), absenteeism.getPayPeriod()) != null) {
-            
-                        ps = conn.prepareStatement(QUERY_UPDATE, PreparedStatement.RETURN_GENERATED_KEYS);
-                        ps.setBigDecimal(1, absenteeism.getPercentage());
-                        ps.setInt(2, absenteeism.getEmployee().getId());
-                        ps.setDate(3, Date.valueOf(absenteeism.getPayPeriod()));
-                        
-                    } else { // If record does not exists already
-                        LocalDate payPeriod = absenteeism.getPayPeriod().with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
-                        
-                        ps = conn.prepareStatement(QUERY_CREATE, PreparedStatement.RETURN_GENERATED_KEYS);
-                        ps.setInt(1, absenteeism.getEmployee().getId());
-                        ps.setDate(2, Date.valueOf(payPeriod));
-                        ps.setBigDecimal(3, absenteeism.getPercentage());
-                    }
-                   /**
-                    * <p> JAVADOC - FIELD </p>
-                    * @author wahic
-                    */
-                    int result = ps.executeUpdate();
-                    if (result > 0) {
+            if (conn.isValid(0)) {
 
-                        rs = ps.getGeneratedKeys();
-                        if (rs.next()) {
-                            key = rs.getInt(1);
-                        }
+                // Find if record already exists
+                if (find(absenteeism.getEmployee(), absenteeism.getPayPeriod()) != null) {
 
-                    }
+                    ps = conn.prepareStatement(QUERY_UPDATE, PreparedStatement.RETURN_GENERATED_KEYS);
+                    ps.setBigDecimal(1, absenteeism.getPercentage());
+                    ps.setInt(2, absenteeism.getEmployee().getId());
+                    ps.setDate(3, Date.valueOf(absenteeism.getPayPeriod()));
+
+                } else { // If record does not exists already
+                    LocalDate payPeriod = absenteeism.getPayPeriod().with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
+
+                    ps = conn.prepareStatement(QUERY_CREATE, PreparedStatement.RETURN_GENERATED_KEYS);
+                    ps.setInt(1, absenteeism.getEmployee().getId());
+                    ps.setDate(2, Date.valueOf(payPeriod));
+                    ps.setBigDecimal(3, absenteeism.getPercentage());
                 }
+                /**
+                 * <p>
+                 * JAVADOC - FIELD </p>
+                 *
+                 * @author wahic
+                 */
+                int result = ps.executeUpdate();
+                if (result > 0) {
 
-            } catch (SQLException e) {
-
-                throw new DAOException(e.getMessage());
-
-            } finally {
-                if (rs != null) {
-                    try {
-                        rs.close();
-                    } catch (SQLException e) {
-                        throw new DAOException(e.getMessage());
+                    rs = ps.getGeneratedKeys();
+                    if (rs.next()) {
+                        key = rs.getInt(1);
                     }
+
                 }
-                if (ps != null) {
-                    try {
-                        ps.close();
-                    } catch (SQLException e) {
-                        throw new DAOException(e.getMessage());
-                    }
+            }
+
+        } catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
                 }
-            }         
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+            }
+        }
         /**
-         * <p> JAVADOC - TYPE </p>
+         * <p>
+         * JAVADOC - TYPE </p>
+         *
          * @author wahic
-        */
+         */
         return key;
-        
+
     }
-    
+
 }
