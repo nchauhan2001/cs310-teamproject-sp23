09f1b65cf3478c0689feb8f8b0f658c0aa4ca620
unknown <wahickssware84@gmail.com>
2023-04-14 12:10:47 -0500

    Updated Javadocs for DAOUtility.java & ReportDAO.java classes.

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/DAOUtility.java
@@ -12,24 +12,27 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 /**
- * 
- * Utility class for DAOs.  This is a final, non-constructable class containing
+ *
+ * <p>
+ * Utility class for DAOs. This is a final, non-constructable class containing
  * common DAO logic and other repeated and/or standardized code, refactored into
- * individual static methods.
- * 
+ * individual static methods. </p>
+ *
+ *
+ * @author wahic
  */
 public final class DAOUtility {
-    
+
     public static String getPunchListAsJSON(ArrayList<Punch> punchList) {
- 
+
         String dateFormat = "E MM/dd/yyyy HH:mm:ss";
-        
+
         /* Create ArrayList Object */
         ArrayList<HashMap<String, String>> jsonData = new ArrayList<>();
 
         /* Create HashMap Object (one for every Punch!) */
-        for (int i = 0; i < punchList.size(); i++){
-            HashMap<String, String>  punchData = new HashMap<>();
+        for (int i = 0; i < punchList.size(); i++) {
+            HashMap<String, String> punchData = new HashMap<>();
             Punch punch = punchList.get(i);
 
             /* Add Punch Data to HashMap */
@@ -38,10 +41,10 @@ public final class DAOUtility {
             punchData.put("id", String.valueOf(punch.getId()));
             punchData.put("punchtype", punch.getPunchType().toString());
             punchData.put("adjustmenttype", punch.getAdjustmentType().toString());
-            
+
             String date = DateTimeFormatter.ofPattern(dateFormat).format(punch.getOriginalTimestamp()).toUpperCase();
             punchData.put("originaltimestamp", date);
-            
+
             String adjustedTime = DateTimeFormatter.ofPattern(dateFormat).format(punch.getAdjustedTimestamp()).toUpperCase();
             punchData.put("adjustedtimestamp", adjustedTime);
 
@@ -50,40 +53,48 @@ public final class DAOUtility {
         }
         return Jsoner.serialize(jsonData);
     }
-    
+
+    /**
+     * <p>
+     * JAVADOC METHOD </p>
+     *
+     * @param punchlist
+     * @param shift
+     * @return
+     */
     public static int calculateTotalMinutes(ArrayList<Punch> punchlist, Shift shift) {
-        
+
         int total = 0; // Total for all the punches
-       
+
         int dailyTotal = 0; // Total minutes for each day
         boolean punchedOutForLunch = false;
         boolean punchedInForLunch = false;
-        
+
         LocalDate currentDate = punchlist.get(0).getAdjustedTimestamp().toLocalDate(); // The current date of the punches we are working with
         DayOfWeek dayOfWeek = currentDate.getDayOfWeek();
-        
-        for(int i = 0; i < punchlist.size(); i++) {
 
-            if(punchlist.size() - 1 > i) { // True if there is a punch after this one
-                    
+        for (int i = 0; i < punchlist.size(); i++) {
+
+            if (punchlist.size() - 1 > i) { // True if there is a punch after this one
+
                 Punch punch = punchlist.get(i);
-                Punch nextPunch = punchlist.get(i+1);
-                
+                Punch nextPunch = punchlist.get(i + 1);
+
                 // True if the current Punch and the next punch are pairs of CLOCK_IN and CLOCK_OUT
-                if(punch.getPunchType().equals(EventType.CLOCK_IN) && nextPunch.getPunchType().equals(EventType.CLOCK_OUT)) {
+                if (punch.getPunchType().equals(EventType.CLOCK_IN) && nextPunch.getPunchType().equals(EventType.CLOCK_OUT)) {
 
                     // If nextPunch isn't on the same date as currentDate, change the currentDate
-                    if(!currentDate.equals(nextPunch.getAdjustedTimestamp().toLocalDate())) {
-                        currentDate = punchlist.get(i).getAdjustedTimestamp().toLocalDate();   
+                    if (!currentDate.equals(nextPunch.getAdjustedTimestamp().toLocalDate())) {
+                        currentDate = punchlist.get(i).getAdjustedTimestamp().toLocalDate();
                     }
-                        
+
                     // Check if the employee clocked out for lunch
-                    if(!punchedOutForLunch && punch.getAdjustmentType() == PunchAdjustmentType.LUNCH_START) {
+                    if (!punchedOutForLunch && punch.getAdjustmentType() == PunchAdjustmentType.LUNCH_START) {
                         punchedOutForLunch = true;
                     }
 
                     // Check if the employee clocked in for lunch
-                    if(!punchedInForLunch && punch.getAdjustmentType() == PunchAdjustmentType.LUNCH_STOP) {
+                    if (!punchedInForLunch && punch.getAdjustmentType() == PunchAdjustmentType.LUNCH_STOP) {
                         punchedInForLunch = true;
                     }
 
@@ -92,86 +103,94 @@ public final class DAOUtility {
                     dailyTotal += diff.toMinutes();
 
                 } else { // They are not pairs of CLOCK_IN and CLOCK_OUT
-                    
-                    if(!nextPunch.getPunchType().equals(EventType.TIME_OUT)) { // The next punch is NOT a TIME_OUT
-                    
+
+                    if (!nextPunch.getPunchType().equals(EventType.TIME_OUT)) { // The next punch is NOT a TIME_OUT
+
                         total += dailyTotal;
-                        
+
                     }
-                    
+
                     dailyTotal = 0;
                     punchedOutForLunch = false;
                     punchedInForLunch = false;
-                    
+
                 }
-                
+
                 // Employee didn't clock in/out for lunch and also met the lunch threshold
-                if(dailyTotal > shift.getLunchThreshold() && !(punchedOutForLunch && punchedInForLunch)) {
-                    
+                if (dailyTotal > shift.getLunchThreshold() && !(punchedOutForLunch && punchedInForLunch)) {
+
                     dailyTotal -= shift.getLunchDuration().toMinutes();
-                    
+
                 }
-                
+
             } else { // There was no next punch. Add the dailyTotal to the total
-                
+
                 total += dailyTotal;
-                
+
             }
-           
+
         }
-       
+
         return total;
-        
+
     }
-    
+
+    /**
+     * <p>
+     * JAVADOC METHOD </p>
+     *
+     * @param punchlist
+     * @param shift
+     * @return
+     */
     public static String getPunchListPlusTotalsAsJSON(ArrayList<Punch> punchlist, Shift shift) {
-        
-        JsonObject result = new JsonObject(); 
+
+        JsonObject result = new JsonObject();
 
         ArrayList<Punch> sortedArray = new ArrayList<>(punchlist);
-        
+
         // Sorting the punchlist into the order that they were punched
-        for(int i = 0; i < sortedArray.size() - 1; i++) {
-            for(int j = 1; j < sortedArray.size() - i - 1; j++) {
-                
+        for (int i = 0; i < sortedArray.size() - 1; i++) {
+            for (int j = 1; j < sortedArray.size() - i - 1; j++) {
+
                 Punch p = sortedArray.get(j);
                 Punch nextP = sortedArray.get(j + 1);
-                
-                if(p.getAdjustedTimestamp().isAfter(nextP.getAdjustedTimestamp())) {
+
+                if (p.getAdjustedTimestamp().isAfter(nextP.getAdjustedTimestamp())) {
                     Punch temp = sortedArray.get(j);
                     sortedArray.set(j, sortedArray.get(j + 1));
                     sortedArray.set(j + 1, temp);
                 }
-                
+
             }
         }
-             
+
         result.put("punchlist", Jsoner.deserialize(getPunchListAsJSON(sortedArray), new JsonArray()));
         result.put("totalminutes", calculateTotalMinutes(sortedArray, shift));
         result.put("absenteeism", calculateAbsenteeism(sortedArray, shift).toString() + "%");
-        
+
         return Jsoner.serialize(result);
-        
+
     }
-    
-    public static BigDecimal calculateAbsenteeism(ArrayList<Punch> punchlist, Shift s){
-        
+
+    public static BigDecimal calculateAbsenteeism(ArrayList<Punch> punchlist, Shift s) {
+
         double scheduledMinutes = 0;
 
-        for(int i = 1; i <= 5; i++){
+        for (int i = 1; i <= 5; i++) {
             Duration lunchDuration = s.getDailyschedule(DayOfWeek.of(i)).getLunchDuration();
             Duration shiftDuration = s.getDailyschedule(DayOfWeek.of(i)).getShiftDuration().minus(lunchDuration);
             scheduledMinutes += shiftDuration.toMinutes();
         }
         BigDecimal percentage;
-        
+
         int totalMinutes = calculateTotalMinutes(punchlist, s);
         double percent = (scheduledMinutes - totalMinutes) / scheduledMinutes * 100;
-        
+
         percentage = new BigDecimal(percent);
-        
+
         percentage = percentage.setScale(2, RoundingMode.HALF_UP);
-        
+
         return percentage;
     }
 }
--- src/edu/jsu/mcis/cs310/tas_sp23/dao/ReportDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/ReportDAO.java
@@ -8,24 +8,36 @@ import edu.jsu.mcis.cs310.tas_sp23.Department;
 import edu.jsu.mcis.cs310.tas_sp23.EmployeeType;
 import java.sql.*;
 
+/**
+ * <p>
+ * CLASS DESCRIPTION </p>
+ *
+ * @author wahic
+ */
 public class ReportDAO {
-    
+
     private static final String QUERY_SUMMARY = "SELECT * FROM employee WHERE departmentid = ? ORDER BY lastname, firstname, middlename";
     private static final String QUERY_SUMMARY2 = "SELECT * FROM employee ORDER BY lastname, firstname, middlename";
-    
+
     private final DAOFactory daoFactory;
 
+    /**
+     * <p>
+     * METHOD DESCRIPTION </p>
+     *
+     * @param daoFactory
+     */
     ReportDAO(DAOFactory daoFactory) {
 
         this.daoFactory = daoFactory;
 
     }
-    
-    public String getBadgeSummary(Object id){
+
+    public String getBadgeSummary(Object id) {
         String s = "";
-        
+
         JsonArray jsonArray = new JsonArray();
-        
+
         PreparedStatement ps = null;
         ResultSet rs = null;
         boolean hasresults;
@@ -35,17 +47,16 @@ public class ReportDAO {
             Connection conn = daoFactory.getConnection();
 
             if (conn.isValid(0)) {
-                
-                if (id == null){
+
+                if (id == null) {
                     ps = conn.prepareStatement(QUERY_SUMMARY2);
                     hasresults = ps.execute();
-                }
-                else{
+                } else {
                     ps = conn.prepareStatement(QUERY_SUMMARY);
                     ps.setInt(1, Integer.parseInt(id.toString()));
                     hasresults = ps.execute();
                 }
-                
+
                 if (hasresults) {
 
                     rs = ps.getResultSet();
@@ -57,29 +68,27 @@ public class ReportDAO {
                         BadgeDAO badgedao = daoFactory.getBadgeDAO();
                         Badge badge = badgedao.find(badgeid);
                         int departmentID;
-                        
-                        if (id != null){
+
+                        if (id != null) {
                             departmentID = Integer.parseInt(id.toString());
-                        }
-                        else{
+                        } else {
                             departmentID = rs.getInt("departmentid");
                         }
-                        
+
                         DepartmentDAO departmentdao = daoFactory.getDepartmentDAO();
-                        
+
                         Department department = departmentdao.find(departmentID);
                         jsonObject.put("department", department.getDescription());
                         jsonObject.put("name", badge.getDescription());
                         jsonObject.put("badgeid", badgeid);
-                        
+
                         int employeeTypeid = rs.getInt("employeetypeid");
                         EmployeeType employeeType = EmployeeType.values()[employeeTypeid];
 
                         String type;
-                        if (employeeType.equals(EmployeeType.FULL_TIME)){
+                        if (employeeType.equals(EmployeeType.FULL_TIME)) {
                             type = "Full-Time Employee";
-                        }
-                        else{
+                        } else {
                             type = "Temporary Employee";
                         }
 
@@ -113,8 +122,8 @@ public class ReportDAO {
             }
 
         }
-        
+
         return s;
     }
-    
+
 }
